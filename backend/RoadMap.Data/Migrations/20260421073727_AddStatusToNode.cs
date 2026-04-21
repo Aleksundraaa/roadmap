def6680f05f1f338519342cf798bd2cbf8f70715
Aleksundraaa <ladycaxap@gmail.com>
2026-04-21 14:38:01 +0500

    миграции

---
+++ backend/RoadMap.Data/Migrations/20260421073727_AddStatusToNode.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadMap.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusToNode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Status",
+                table: "Nodes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Nodes");
+        }
+    }
+}
