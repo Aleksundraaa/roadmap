060799642dcb98dc91fdc8b9fed5b76bc6eafabe
Aleksundraaa <ladycaxap@gmail.com>
2026-05-11 17:14:57 +0500

    а ну и сами миграции

---
+++ backend/RoadMap.Data/Migrations/20260511104115_AddUserTable.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadMap.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Roadmaps",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(type: "TEXT", nullable: false),
+                    Password = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roadmaps_UserId",
+                table: "Roadmaps",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Roadmaps_Users_UserId",
+                table: "Roadmaps",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Roadmaps_Users_UserId",
+                table: "Roadmaps");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Roadmaps_UserId",
+                table: "Roadmaps");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Roadmaps");
+        }
+    }
+}
